7b98f4e12b8571e7fa113be886e82cde24d86859
KnighT178 <egor.skandakov@gmail.com>
2022-11-19 02:03:27 +0300

    Added following mechanism

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   get '/users/:id/blog', to: 'diaries#show', as: :blog
 
+  post '/users/:id/subscribe', to: 'subscriptions#change_subscription_status', as: :subscription
+  get 'subscriptions/posts', to: 'posts#subscription_index', as: :subscriptions_posts
+
   resources :diaries, except: %i[show new edit index] do
     resources :posts, only: %i[create destroy update]
   end
