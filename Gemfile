3633c5fab0e4a424c8678c768c90c76402a73949
KnighT178 <egor.skandakov@gmail.com>
2022-11-19 03:40:45 +0300

    Added the ability for user to like posts

--- Gemfile
+++ Gemfile
@@ -26,11 +26,11 @@ gem 'jbuilder', '~> 2.7'
 gem 'image_processing', '~> 1.2'
 
 gem 'aws-sdk-s3', require: false
-gem 'bootsnap', '>= 1.4.4', require: false
 gem 'bootstrap'
 gem 'jquery-rails'
 gem 'devise'
 gem 'slim-rails'
+gem 'acts_as_votable'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,5 +8,7 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
 
-  scope :subscribed_by, ->(user) { where(author_id: Subscription.select(:followed_blogger_id).where(follower_id: user.id)).order(title: :desc) }
+  scope :subscribed_by, ->(user) { where(author_id: Subscription.select(:followed_blogger_id).where(follower_id: user.id)).order(created_at: :desc) }
+
+  acts_as_votable
 end
